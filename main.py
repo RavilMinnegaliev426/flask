f3ec536993365250f3eb02793ba5763fd893e0e6
ravkami@yandex.ru <ravkami@yandex.ru>
2025-04-22 21:25:46 +0300

    it-3 get/put

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -29,10 +30,39 @@ def submit():
     return f'Hello, {name}'
 
 
-# @app.route('/data')
-# def data():
-#     return jsonify({'key': 'value'})
+@app.route('/data')
+def data():
+    return jsonify({'key': 'value'})
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# sql
+# подключение базы данных
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+db.create_all()
+
+user = User.query.first()
+print(user)
+
+
+new_user = User(username='newuser', email='email@example.com')
+db.session.add(new_user)
+db.session.commit()
+
+
+User.query.filter_by(username='newuser').first()
